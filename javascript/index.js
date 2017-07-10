fbcbf608b53a19b22eac8f1b9daa6cdb86b611e1
Sebastian Odell <sebastian@sanctuary.computer>
2017-07-10 19:02:14 -0400

    sets up waypoints

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,31 @@
-console.log('hi')
+//Homepage Scroll interaction
+let $homeBlocks = $('.home-block');
+
+$homeBlocks.each((i, element) => {
+  let $homeBlock = $(element);
+
+
+  //Peek Next
+  $homeBlock.waypoint({
+    handler: function(direction) {
+      let first = this.group.first();
+      let next = this.next();
+
+      debugger;
+      if (next !== first) {
+        $(next.element).addClass('next-in-line');
+      }
+    },
+    offset: '50%',
+    group: 'home-middle'
+  });
+
+  //Scroll Next
+  $homeBlock.waypoint({
+    handler: function(direction) {
+      console.log('waypoint 1')
+    },
+    offset: '2%',
+    group: 'home-bottom'
+  });
+})
--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,6 +1,35 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
-console.log('hi');
+//Homepage Scroll interaction
+var $homeBlocks = $('.home-block');
+
+$homeBlocks.each(function (i, element) {
+  var $homeBlock = $(element);
+
+  //Peek Next
+  $homeBlock.waypoint({
+    handler: function handler(direction) {
+      var first = this.group.first();
+      var next = this.next();
+
+      debugger;
+      if (next !== first) {
+        $(next.element).addClass('next-in-line');
+      }
+    },
+    offset: '50%',
+    group: 'home-middle'
+  });
+
+  //Scroll Next
+  $homeBlock.waypoint({
+    handler: function handler(direction) {
+      console.log('waypoint 1');
+    },
+    offset: '2%',
+    group: 'home-bottom'
+  });
+});
 
 },{}]},{},[1]);
