5fefe4826507c6b024ba8be8c0141d176fe23558
Sebastian Odell <sebastian@sanctuary.computer>
2017-07-11 15:20:23 -0400

    dispables waypoints on mobile device

--- javascript/index.js
+++ javascript/index.js
@@ -1,28 +1,60 @@
-//Homepage Scroll interaction
-let $homeBlocks = $('.home-block');
+const isMobile = /iphone|ipod|ipad|android|blackberry|opera mini|opera mobi|skyfire|maemo|windows phone|palm|iemobile|symbian|symbianos|fennec/i.test(navigator.userAgent.toLowerCase());
 
-$($homeBlocks[0]).find('.title-bar').addClass('next');
+/**
+// Only apply waypoints
+// interaction if not mobile
+**/
 
-$homeBlocks.each((i, element) => {
-  let $homeBlock = $(element);
+if (!isMobile) {
+  //Homepage Scroll interaction
+  let $homeBlocks = $('.home-block');
+  let $introTrigger = $('.intro-trigger');
+  const TITLE_BAR_HEIGHT = 125;
 
-  //Peek Next
-  $homeBlock.waypoint({
+  $($homeBlocks[0]).find('.title-bar').addClass('next');
+
+  $homeBlocks.each((i, element) => {
+    console.log(isMobile)
+    let $homeBlock = $(element);
+
+    //Peek Next
+    $homeBlock.waypoint({
+      handler: function(direction) {
+        if (direction === 'down') {
+          $(this.element).find('.title-bar').removeClass('next');
+        }
+
+        if (direction === 'up') {
+          $('.title-bar').removeClass('next');
+          $(this.element).find('.title-bar').addClass('next');
+        }
+      },
+      offset: function() {
+        return $(window).innerHeight() - (TITLE_BAR_HEIGHT/2);
+      }
+    });
+
+    //Scroll Next
+    $homeBlock.waypoint({
+      handler: function(direction) {
+        if (this !== this.group.last()) {
+          let next = this.next().element;
+          $(next).find('.title-bar').addClass('next');
+        }
+      },
+      offset: 25,
+    });
+  });
+
+  //Back to top
+  $introTrigger.waypoint({
     handler: function(direction) {
-      $(this.element).find('.title-bar').removeClass('next');
+      if (direction === 'down') {
+        window.scrollTo(0, 0);
+      }
     },
     offset: function() {
-      return $(window).innerHeight() - (125/2);
+      return TITLE_BAR_HEIGHT/2;
     }
-  });
-
-  //Scroll Next
-  $homeBlock.waypoint({
-    handler: function(direction) {
-      let next = this.next().element;
-      $(next).find('.title-bar').addClass('next');
-    },
-    offset: 25,
-    group: 'home-bottom'
-  });
-})
+  })
+};
--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,33 +1,65 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
-//Homepage Scroll interaction
-var $homeBlocks = $('.home-block');
+var isMobile = /iphone|ipod|ipad|android|blackberry|opera mini|opera mobi|skyfire|maemo|windows phone|palm|iemobile|symbian|symbianos|fennec/i.test(navigator.userAgent.toLowerCase());
 
-$($homeBlocks[0]).find('.title-bar').addClass('next');
+/**
+// Only apply waypoints
+// interaction if not mobile
+**/
 
-$homeBlocks.each(function (i, element) {
-  var $homeBlock = $(element);
+if (!isMobile) {
+  //Homepage Scroll interaction
+  var $homeBlocks = $('.home-block');
+  var $introTrigger = $('.intro-trigger');
+  var TITLE_BAR_HEIGHT = 125;
 
-  //Peek Next
-  $homeBlock.waypoint({
+  $($homeBlocks[0]).find('.title-bar').addClass('next');
+
+  $homeBlocks.each(function (i, element) {
+    console.log(isMobile);
+    var $homeBlock = $(element);
+
+    //Peek Next
+    $homeBlock.waypoint({
+      handler: function handler(direction) {
+        if (direction === 'down') {
+          $(this.element).find('.title-bar').removeClass('next');
+        }
+
+        if (direction === 'up') {
+          $('.title-bar').removeClass('next');
+          $(this.element).find('.title-bar').addClass('next');
+        }
+      },
+      offset: function offset() {
+        return $(window).innerHeight() - TITLE_BAR_HEIGHT / 2;
+      }
+    });
+
+    //Scroll Next
+    $homeBlock.waypoint({
+      handler: function handler(direction) {
+        if (this !== this.group.last()) {
+          var next = this.next().element;
+          $(next).find('.title-bar').addClass('next');
+        }
+      },
+      offset: 25
+    });
+  });
+
+  //Back to top
+  $introTrigger.waypoint({
     handler: function handler(direction) {
-      $(this.element).find('.title-bar').removeClass('next');
+      if (direction === 'down') {
+        window.scrollTo(0, 0);
+      }
     },
     offset: function offset() {
-      return $(window).innerHeight() - 125 / 2;
+      return TITLE_BAR_HEIGHT / 2;
     }
   });
-
-  //Scroll Next
-  $homeBlock.waypoint({
-    handler: function handler(direction) {
-      var next = this.next().element;
-      $(next).find('.title-bar').addClass('next');
-    },
-    offset: 25,
-    group: 'home-bottom'
-  });
-});
+};
 
 },{}]},{},[1]);
