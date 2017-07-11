7fa4a5b63fc7748a14ef7b5788c1582f4a9c4c2d
Sebastian Odell <sebastian@sanctuary.computer>
2017-07-11 15:22:37 -0400

    cleanup"

--- javascript/utilities.js
+++
@@ -1,1 +0,0 @@
-export const isMobile = /iphone|ipod|ipad|android|blackberry|opera mini|opera mobi|skyfire|maemo|windows phone|palm|iemobile|symbian|symbianos|fennec/i.test(navigator.userAgent.toLowerCase());
